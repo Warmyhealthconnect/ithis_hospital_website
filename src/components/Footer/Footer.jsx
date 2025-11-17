7cbab2e7a60e58d82e32b70ae483cdb4ec135311
Vinukrishna05 <vinukrishna0508@gmail.com>
2025-11-17 13:36:18 +0530

    vk

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,47 +1,42 @@
 import React from 'react'
 import './Footer.css'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="container footer__grid">
-        <div className="footer__col">
-          <h4>Company</h4>
-          <ul>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Careers</a></li>
-            <li><a href="#">News</a></li>
+      <div className='row'>
+        <div className="col">
+          <ul className="navbar-list ">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/">About</Link></li>
+            <li><Link to="/">Specialities</Link></li>
+            <li><Link to="/">Doctors</Link></li>
           </ul>
         </div>
-        <div className="footer__col">
-          <h4>Departments</h4>
-          <ul>
-            <li><a href="#">Cardiology</a></li>
-            <li><a href="#">Orthopedics</a></li>
-            <li><a href="#">Pediatrics</a></li>
+        <div className="col">
+          <ul className="navbar-list ">
+            <li><Link to="/">Specialities</Link></li>
+            <li><Link to="/">About</Link></li>
+            <li><Link to="/">Specialities</Link></li>
+            <li><Link to="/">Doctors</Link></li>
           </ul>
         </div>
-        <div className="footer__col">
-          <h4>Support</h4>
-          <ul>
-            <li><a href="#">Help Center</a></li>
-            <li><a href="#">Privacy</a></li>
-            <li><a href="#">Terms</a></li>
+        <div className="col">
+          <ul className="navbar-list ">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/">About</Link></li>
+            <li><Link to="/">Specialities</Link></li>
+            <li><Link to="/">Doctors</Link></li>
           </ul>
         </div>
-        <div className="footer__col">
-          <h4>Contact</h4>
-          <p>+91 00000 00000</p>
-          <p>info@ithis.com</p>
-          <p>123 Health Street</p>
-        </div>
-      </div>
-      <div className="footer__bottom">
-        <div className="container footer__bottomInner">
-          <div className="footer__brand">
-            <span className="brand__logo">i</span><span className="brand__text">this</span>
-          </div>
-          <div className="footer__copy">© 2025 Ithis Hospital</div>
+        <div className="col">
+          <ul className="navbar-list ">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/">About</Link></li>
+            <li><Link to="/">Specialities</Link></li>
+            <li><Link to="/">Doctors</Link></li>
+          </ul>
         </div>
       </div>
     </footer>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,32 @@ import './Header.css'
 
 export default function Header() {
   return (
-    <header className="header row" style={{width:"100vw", height:"20vh"}}>
-      <div className="col-3">
-        <img src="/logo 1.png" className='img-fluid' alt="" />
-      </div>
-      <div className="col">
-        
-      </div>
-    </header>
+      <header className="header row px-5 mt-4">
+        <div className="col-4">
+          <img src="/logo 1.png" className='img-fluid' alt="" />
+        </div>
+        <div className="col">
+          <div className="row gap-2">
+            {/* Left Column */}
+            <div className="col leftlist border rounded-3 d-flex py-2 px-0">
+              <ul className="navbar-list d-flex justify-content-between my-auto w-100">
+                <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">About</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Specialities</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Doctors</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Birth companion suite</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Home Care Service</a></li>
+              </ul>
+            </div>
+
+            {/* Right Column */}
+            <div className="col-2 btn d-flex justify-content-center align-items-center rounded-3 contact-btn">  
+              <span className="text-white bold">Contact Us</span>
+            </div>
+
+          </div>
+
+        </div>
+      </header>
   )
 }
