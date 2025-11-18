cf2e0cca3731e08bb1ab44a07cfe3a024f33a4db
Vinukrishna05 <vinukrishna0508@gmail.com>
2025-11-18 13:47:48 +0530

    vk

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,40 +5,49 @@ import { Link } from 'react-router-dom'
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className='row'>
+      <div className='row px-5 py-4'>
+
         <div className="col">
-          <ul className="navbar-list ">
+          <h5 className="footer-title">Quick Links</h5>
+          <ul className="navbar-list">
             <li><Link to="/">Home</Link></li>
             <li><Link to="/">About</Link></li>
-            <li><Link to="/">Specialities</Link></li>
+            <li><Link to="/">Facilities</Link></li>
             <li><Link to="/">Doctors</Link></li>
           </ul>
         </div>
+
         <div className="col">
-          <ul className="navbar-list ">
+          <h5 className="footer-title">Services</h5>
+          <ul className="navbar-list">
             <li><Link to="/">Specialities</Link></li>
-            <li><Link to="/">About</Link></li>
-            <li><Link to="/">Specialities</Link></li>
-            <li><Link to="/">Doctors</Link></li>
+            <li><Link to="/">Home Care Service</Link></li>
+            <li><Link to="/">Birth Companion Suite</Link></li>
+            <li><Link to="/">Gallery</Link></li>
           </ul>
         </div>
+
         <div className="col">
-          <ul className="navbar-list ">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/">About</Link></li>
-            <li><Link to="/">Specialities</Link></li>
-            <li><Link to="/">Doctors</Link></li>
+          <h5 className="footer-title">More</h5>
+          <ul className="navbar-list">
+            <li><Link to="/">Career</Link></li>
+            <li><Link to="/">Contact</Link></li>
+            <li><Link to="/">Mom to Be</Link></li>
+            <li><Link to="/">Mother Hood</Link></li>
           </ul>
         </div>
+
         <div className="col">
-          <ul className="navbar-list ">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/">About</Link></li>
-            <li><Link to="/">Specialities</Link></li>
-            <li><Link to="/">Doctors</Link></li>
+          <h5 className="footer-title">Contact Info</h5>
+          <ul className="navbar-list">
+            <li><Link to="/">ithishospital@gmail.com</Link></li>
+            <li><Link to="/">+91 483 3500300</Link></li>
+            <li><Link to="/">Vengara Road, Kizhakkethala, Malappuram, Kerala, 676505</Link></li>
           </ul>
         </div>
+
       </div>
     </footer>
+
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,32 +3,62 @@ import './Header.css'
 
 export default function Header() {
   return (
-      <header className="header row px-5 mt-4">
-        <div className="col-4">
-          <img src="/logo 1.png" className='img-fluid' alt="" />
-        </div>
-        <div className="col">
-          <div className="row gap-2">
-            {/* Left Column */}
-            <div className="col leftlist border rounded-3 d-flex py-2 px-0">
-              <ul className="navbar-list d-flex justify-content-between my-auto w-100">
-                <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">About</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">Specialities</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">Doctors</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">Birth companion suite</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">Home Care Service</a></li>
-              </ul>
-            </div>
+    <header className="header row px-md-5 mt-4 mx-0">
 
-            {/* Right Column */}
-            <div className="col-2 btn d-flex justify-content-center align-items-center rounded-3 contact-btn">  
-              <span className="text-white bold">Contact Us</span>
-            </div>
+      {/* Logo */}
+      <div className="col-2 d-flex align-items-center">
+        <img src="/logo 1.png" className='img-fluid h-50' alt="" />
+      </div>
 
+      {/* Desktop Menu */}
+      <div className="col d-none d-md-block">
+        <div className="row gap-2">
+          <div className="col-1"></div>
+
+          <div className="col leftlist border d-flex py-2 px-0">
+            <ul className="navbar-list d-flex justify-content-between my-auto w-100">
+              <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/specialities">Specialities</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/doctors">Doctors</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/birth-companion-suite">Birth Companion Suite</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/home-care-service">Home Care Service</Link></li>
+
+            </ul>
           </div>
 
+          <div className="col-2 btn d-flex justify-content-center align-items-center contact-btn">
+            <span className="text-white bold">Contact Us</span>
+          </div>
         </div>
-      </header>
+      </div>
+
+      {/* Mobile Hamburger (visible only on mobile) */}
+      <div className="col d-flex justify-content-end align-items-center d-md-none">
+        <button
+          className="hamburger-btn"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mobileMenu"
+        >
+          ☰
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      <div className="collapse mobile-menu mt-3" id="mobileMenu">
+        <ul className="mobile-list">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Specialities</a></li>
+          <li><a href="#">Doctors</a></li>
+          <li><a href="#">Birth companion suite</a></li>
+          <li><a href="#">Home Care Service</a></li>
+          <li className="mobile-contact">Contact Us</li>
+        </ul>
+      </div>
+
+    </header>
+
   )
 }
